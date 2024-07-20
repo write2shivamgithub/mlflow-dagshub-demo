f6a807e10e6c066b166239de1d8c3950b4a912e1
write2shivamgithub <shivamshankaraacharya@gmail.com>
2024-07-21 01:41:57 +0530

    how to log dataset using mlflow

--- log_dataset.py
+++ log_dataset.py
@@ -1,5 +1,5 @@
+import pandas as pd
 import mlflow
-from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
@@ -12,9 +12,10 @@ dagshub.init(repo_owner='write2shivamgithub', repo_name='mlflow-dagshub-demo', m
 mlflow.set_tracking_uri("https://dagshub.com/write2shivamgithub/mlflow-dagshub-demo.mlflow")
 
 # Load the iris dataset
-iris = load_iris()
-X = iris.data
-y = iris.target
+iris = pd.read_csv("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv")
+
+X = iris.iloc[:, 0:-1]
+y = iris.iloc[:, -1]
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -24,21 +25,21 @@ max_depth = 15
 n_estimators = 150
 
 # Apply mlflow
-mlflow.set_experiment('iris_rf')
-with mlflow.start_run(run_name="Team_exp"):
+mlflow.set_experiment('log_dataset')
+with mlflow.start_run(run_name="Shankaraacharya"):
     rf = RandomForestClassifier(max_depth=max_depth)
     rf.fit(X_train, y_train)
     y_pred = rf.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
-    
+
     mlflow.log_metric('accuracy', accuracy)
     mlflow.log_param('max_depth', max_depth)
     mlflow.log_param('n_estimators', n_estimators)
 
     # Create a confusion matrix
-    cm =confusion_matrix(y_test, y_pred)
-    plt.figure(figsize=(6,6))
-    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels= iris.target_names, yticklabels=iris.target_names)
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6, 6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['setosa', 'versicolor', 'virginica'], yticklabels=['setosa', 'versicolor', 'virginica'])
     plt.ylabel('Actual')
     plt.xlabel('Predicted')
     plt.title('Confusion Matrix')
@@ -47,18 +48,27 @@ with mlflow.start_run(run_name="Team_exp"):
     plt.savefig('confusion_matrix.png')
     mlflow.log_artifact('confusion_matrix.png')
 
-    #log code
+    # Log code
     mlflow.log_artifact(__file__)
 
-    #log model
-    mlflow.sklearn.log_model(rf,'random_forest')
+    # Log model
+    mlflow.sklearn.log_model(rf, 'random_forest')
 
-    #log tag
-    mlflow.set_tag('author',"kanak")
-    mlflow.set_tag('model','random_forest')
-    
+    # Log tags
+    mlflow.set_tag('author', "Bushra")
+    mlflow.set_tag('model', 'random_forest')
+
+    # Log dataset (as pandas DataFrame)
+    train_df = X_train
+    train_df['species'] = y_train
+
+    test_df = X_test
+    test_df['species'] = y_test
+
+    train_df = mlflow.data.from_pandas(train_df)
+    test_df = mlflow.data.from_pandas(test_df)
+
+    mlflow.log_input(train_df, 'train')
+    mlflow.log_input(test_df, 'validation')
+  
     print('Accuracy:', accuracy)
-
-
-
-
